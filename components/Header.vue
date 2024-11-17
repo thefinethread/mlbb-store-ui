9f49739fdf7bf062a626ce6dd94218a1093aa3ad
Kartikey Shukla <kartikey8604@gmail.com>
2024-11-17 19:28:32 +0530

    project initialization

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<header class="bg-neutral-950 h-20 w-full fixed top-0 left-0">
+		<div class="w-full h-full flex justify-between items-center px-8">
+			<NuxtImg src="/svgs/store-logo.svg" class="h-10 w-10" />
+
+			<div class="flex gap-6 items-center justify-between">
+				<ul>
+					<li class="cursor-pointer">
+						<NuxtImg src="/svgs/search-icon.svg" class="h-6 w-6" />
+					</li>
+				</ul>
+				<div class="cursor-pointer">Login</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script setup></script>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="bg-neutral-900 min-h-dvh w-screen text-neutral-50">
+		<Header />
+		<slot />
+	</div>
+</template>
